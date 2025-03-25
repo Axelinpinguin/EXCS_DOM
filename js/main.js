959d77dc9dcb1a31908380ecf8ff5f1ccc322957
Axelinpinguin <axel.0302@outlook.es>
2025-03-25 12:04:26 -0600

    Documentando el codigo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let lista=document.getElementById('lista')
+//manejando el item de peras
 let nodo1=document.createElement('li');
-lista.appendChild(nodo1);
 let peras=document.createTextNode('peras');
-nodo1.appendChild(peras);
+//manejando el item manzanas
 let nodo2=document.createElement('li');
-lista.appendChild(nodo2);
 let manzanas=document.createTextNode('manzanas');
+//obteniendo listas y agregando nodos
+lista.appendChild(nodo1);
+lista.appendChild(nodo2);
+nodo1.appendChild(peras);
 nodo2.appendChild(manzanas);
