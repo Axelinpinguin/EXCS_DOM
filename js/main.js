1a03e7faa015a40c8e6f5aafd893e7fa9d0b6bd8
Axelinpinguin <axel.0302@outlook.es>
2025-03-25 11:58:28 -0600

    Ejercicio con el DOM (inyeccion de elementos)

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let lista=document.getElementById('lista')
+let nodo1=document.createElement('li');
+lista.appendChild(nodo1);
+let peras=document.createTextNode('peras');
+nodo1.appendChild(peras);
+let nodo2=document.createElement('li');
+lista.appendChild(nodo2);
+let manzanas=document.createTextNode('manzanas');
+nodo2.appendChild(manzanas);
